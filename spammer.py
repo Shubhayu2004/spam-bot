3b538a8119968b5f0b43c9fa23793f31150f7be9
Shubhayu Sarkar <66989335+Shubhayu2004@users.noreply.github.com>
2020-09-30 08:53:32 +0530

    Add files via upload

---
+++ spammer.py
@@ -0,0 +1,213 @@
+We're going 0900 at J-Gate.
+
+
+
+  
+  
+  What do you think, buzzy-boy?
+  
+  
+  Dad, you surprised me.
+  
+  
+  
+    
+    
+    You decide what you're interested in?
+    
+      
+      
+      - Well, there's a lot of choices.
+      
+      - But you only get one.
+      
+      
+      
+      - I'm going out.
+      
+      - Out? Out where?
+      
+      
+      
+        
+        
+        - Out there.
+        import pyautogui , time
+
+
+
+to make one d
+
+
+  
+
+  Like what? Give me one example.
+
+
+
+    
+
+
+    Right.
+      
+
+      Perhaps. Unless you're wearing it
+      
+      and the ladies see you wearing it.
+      
+      
+      
+        
+        
+        Those ladies?
+        
+        Aren't they our cousins too?
+        
+        
+        
+          
+          
+          Distant. Distant.
+          
+          
+          
+            
+            
+            Look at these two.
+            
+            
+            
+              
+              
+              - Oouple of Hive Harrys.
+              
+              - Let's have fun with them.
+              
+              
+              
+                
+                
+                It must be dangerous
+                
+                being a Pollen Jock.
+                
+                
+                
+                  
+                  
+                  Yeah. Once a bear pinned me
+                  
+                  against a mushroom!
+                  
+                  
+                    
+                    
+                    He had a paw on my throat,
+                    
+                    and with the other, he was slapping me!
+                    
+                    
+                    
+                      
+                      
+                      - Oh, my!
+                      
+                      - I never I'd knock him out.
+                      
+                      
+                      
+                        
+                        
+                        What were you doing during this?
+                        
+                        
+                          
+                          
+                          Trying to alert the authorities.
+                          
+                          
+                          
+                            
+                            
+                            I can autograph that.
+                            
+                            
+                            
+                            
+                            A little gusty out there today,
+                            
+                            wasn't it, comrades?
+                            
+                            
+                              
+                              
+                              Yeah. Gusty.
+                              
+                              
+                                
+                                
+                                We're hitting a sunflower patch
+                                
+                                six miles from here tomorrow.
+                                
+                                
+                                  
+                                  
+                                  - Six miles, huh?
+                                  
+                                  - Barry!
+                                  
+                                  
+                                  
+                                    
+                                    
+                                    A puddle jump for us,
+                                    
+                                    but maybe you're not up for it.
+                                    
+                                    
+                                    
+                                      
+                                      
+                                      - Maybe I am.
+                                      
+                                      ''
+time.sleep(5)
+f = open('spamming', 'r')
+for i in f:
+    pyautogui.typewrite(i)
+    pyautogui.press('enter')
+
+a new helmet technology.
+
+
+
+  
+
+  - What do you think he makes?
+
+  - Not enough.
+
+
+
+    
+
+    Here we have our latest advancement,
+
+    the Krelman.
+
+
+
+      
+
+      - What does that do?
+
+      - Oatches that little strand of honey
+
+
+
+        
+
+        that hangs after you pour it.
+
+        Saves us millions.
